342aa10fc3b4197b6372eaaea1468c24d0d7829c
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-09-07 22:23:51 -0400

    via kodekloud.io (55e891ec6e6f644697180000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -113,7 +113,7 @@ function check_open_time(d){
     var date = new Date();
     var current_time = date.getHours();
     var open = d.getUTCHours();
-    console.log(urrent_time >= open)
+    console.log(current_time >= open)
     if (current_time >= open ){
         time = "OPEN NOW";
     }
